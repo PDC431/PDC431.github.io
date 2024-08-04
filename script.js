f27949fb17a72a3ad14fe17a3c7bf32f95cd8d22
PDC431 <peremiahc@gmail.com>
2024-08-04 11:33:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.fillStyle = 'white';
+ctx.fillStyle = 'pink';
 
 class Ball {
     constructor(effect){
